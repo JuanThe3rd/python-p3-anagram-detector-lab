2c644938cca0b048e9b29ff5c62153a37eeb8eea
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-07-06 15:29:31 -0700

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+
+        for word in words:
+            if sorted(word) == sorted(self.word):
+                anagrams.append(word)
+
+        return anagrams
